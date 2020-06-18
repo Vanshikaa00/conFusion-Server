0fffb28919258a3670a3cacb9329c7e5537e4024
Vanshikaa <vanshika31032000@gmail.com>
2020-06-18 22:27:30 +0530

    Mongoose Population

--- models/dishes.js
+++ models/dishes.js
@@ -13,11 +13,10 @@ const commentSchema = new Schema({
     comment: {
         type: String,
         required: true,
-
     },
     author: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
     }
 }, {
     timestamps: true
